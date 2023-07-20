0d3a31bba785afa70215d6b215728e1168620670
AbelAditya <abeladityaphilipose@gmail.com>
2023-07-20 12:45:07 +0530

    first commit

---
+++ API.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+client = MongoClient("mongodb+srv://abelphilipose:1234567890@cluster-0.fyleujw.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.get_database("first")
+contacts = db.contacts
+
+class contact(BaseModel):
+    name: str
+    cont: list
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "This is home"
+
+@app.post("/addContacts")
+def putData(data:contact):
+    l = contacts.find_one({"user":data.name},{"_id":0})
+    if(l!=None):
+        contacts.update_one({"user":data.name},{"$set":{"contacts":data.cont}})
+    else:
+        contacts.insert_one({"user":data.name,"contacts":data.cont})
+
+    return {"status":True}
+
+
+@app.get("/getContacts")
+def getData(user: str):
+    l = contacts.find_one({"user":user},{"_id":0})
+    if l!=None:
+        return l
+    
+    return {"user":user,"contacts":[]}
+
+
+contacts.update_one({"user":"akshun"},{"$set":{"contacts":["9444052933"]}})
